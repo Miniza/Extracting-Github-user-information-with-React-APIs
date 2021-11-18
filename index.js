b0bc0cea3563aa9e47ad0279b2857dc4e3697d80
Minenhle <67018029+Miniza@users.noreply.github.com>
2021-11-18 12:08:57 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,34 @@
  const {useState, useEffect} = React;
 
   const App = () => {
-  const [item, setItem] = useState('');
+  const [resources, setResources] = useState('repos');
+  const [value, setValue] = useState('Miniza');
   const [list, setList] = useState([]);
-  
+ 
   useEffect(()=>{
-   fetch('https://api.github.com/users/Miniza/following')
+  fetch(`https://api.github.com/users/${value}/${resources}`)
   .then(response => response.json())
   .then(data => setList(data));
 
 
-  },[])
+  },[resources])
+    
+  if(list == "") return `0 ${resources}`;
   
   return(
   <>
+      <input type="text" value={value}  onChange = {e=>setValue(e.target.value)} />
+      <hr></hr> 
+      <button onClick = {() => setResources("following")}>Following</button>
+      <button onClick = {() => setResources("followers")}>Followers</button>
+      <button onClick = {() => setResources("repos")}>Repos</button> 
+      <hr></hr>
+      <h4>{resources}</h4>
       <ul>{list.map(item=>{return(<pre>{JSON.stringify(item)}</pre>)})}</ul>
-  </>
+ </>
+    
   )
                                   
-}
+ }
 
-ReactDOM.render(<App/>, document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById('root'))
