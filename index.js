a72dc4b7781e4072d6284df880bfc267c9a675d8
Minenhle <67018029+Miniza@users.noreply.github.com>
2021-11-18 22:45:09 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,34 +1,39 @@
  const {useState, useEffect} = React;
 
-  const App = () => {
-  const [resources, setResources] = useState('repos');
-  const [value, setValue] = useState('Miniza');
-  const [list, setList] = useState([]);
- 
-  useEffect(()=>{
-  fetch(`https://api.github.com/users/${value}/${resources}`)
-  .then(response => response.json())
-  .then(data => setList(data));
-
-
-  },[resources])
-    
-  if(list == "") return `0 ${resources}`;
-  
+const App = () => {
   return(
-  <>
-      <input type="text" value={value}  onChange = {e=>setValue(e.target.value)} />
-      <hr></hr> 
-      <button onClick = {() => setResources("following")}>Following</button>
-      <button onClick = {() => setResources("followers")}>Followers</button>
-      <button onClick = {() => setResources("repos")}>Repos</button> 
-      <hr></hr>
-      <h4>{resources}</h4>
-      <ul>{list.map(item=>{return(<pre>{JSON.stringify(item)}</pre>)})}</ul>
- </>
-    
-  )
-                                  
+    <div>
+  <h4>Search a user Github Repo</h4>
+  <SearchRepo />
+     </div>
+  ) 
+} 
+const SearchRepo = () => {
+  const [searchrepo,  setSearchRepo] =        useState("");
+  const [list, setList] = useState([]);
+  const handleChange = e => {
+    setSearchRepo(e.target.value) 
+  } 
+  const handleClick = () => {
+ axios.get(`https://api.github.com/users/${searchrepo}/repos`).then(response=>
+      setList(response.data))
+       setSearchRepo("")
+  }
+  console.log(searchrepo)
+  return(
+    <>
+    <input type="text" value = {searchrepo} onChange={handleChange} />
+      <button onClick ={handleClick}>Search</button>
+   <ListRepo list = {list} />
+    </>
+  ) 
+}
+   const ListRepo = (props) => {
+   const { list } = props;
+   
+   return(
+   <ul>{list.map(item=>{return(<li key={item.id} >{item.name}</li>)})}</ul>
+ ) 
  }
-
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById("root")) 
+root")) 
